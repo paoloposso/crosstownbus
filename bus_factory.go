4575713f05eeea05ee2f71af88305e2edfe61caf
Paolo <pvictorsys@gmail.com>
2021-09-18 13:19:50 -0300

    change tests structure

---
+++ bus_factory.go
@@ -0,0 +1,13 @@
+package busfactory
+
+import (
+	"reflect"
+
+	eventbus "github.com/paoloposso/crosstownbus/event_bus"
+	redisbus "github.com/paoloposso/crosstownbus/redis_bus"
+)
+
+func CreateRedisBus(event reflect.Type, uri string, password string) (eventbus.Bus, error) {
+	config := redisbus.RedisConfig{Uri: uri, Password: password}
+	return redisbus.CreateBus(event.Name(), config)
+}
