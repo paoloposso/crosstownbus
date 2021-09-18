eb1bc72887ae936d0abb3abc082c3189f15f347f
Paolo <pvictorsys@gmail.com>
2021-09-18 13:04:25 -0300

    change directories structure

--- rabbitmq_bus/bus.go
+++ rabbitmq_bus/bus.go
@@ -5,7 +5,7 @@ import (
 	"fmt"
 	"log"
 
-	eventbus "github.com/paoloposso/crosstown-bus/src/event_bus"
+	eventbus "github.com/paoloposso/crosstown-bus/event_bus"
 	"github.com/streadway/amqp"
 )
 
